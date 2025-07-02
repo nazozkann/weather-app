727267ded5b572e50f7109b59105f0cb4a6fddde
savetheworld <nazozkandesign@gmail.com>
2025-07-02 13:24:21 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/weather-app/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
